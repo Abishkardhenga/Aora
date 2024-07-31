031e0fedfa126cfd153cbbaa6a94fe882d336d7d
Abishkar dhenga <Aabiskardhenga29@gmai.com>
2024-08-01 01:41:23 +0545

    home page done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -10,53 +10,53 @@ import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
 
 const home = () => {
-  const [ refreshing  ,setRefreshing ] = useState(false)
-  const  {data : posts, refetch } = useAppwrite(GetAllPost)
+  const [refreshing, setRefreshing] = useState(false)
+  const { data: posts, refetch } = useAppwrite(GetAllPost)
   console.log("this is post", posts)
- 
-  const onRefresh =  async ()=>{
+
+  const onRefresh = async () => {
     await refetch()
     setRefreshing(true)
   }
 
- 
+
   return (
     <SafeAreaView className="bg-primary  h-full">
-      <StatusBar style='light'/>
+      <StatusBar style='light' />
       <FlatList
-       data={posts} 
-       keyExtractor={(item) => item.$id} renderItem={({ item }) => {
-        return (
-          <VideoCard video={item}/>
-        )
-      }} ListHeaderComponent={() => (
-        <View className="my-6 px-4 space-y-6">
-          <View className="justify-between mb-6 items-start flex-row">
-            <View>
-              <Text className="text-sm font-pmedium text-gray-100">
-                Welcome Back
+        data={posts}
+        keyExtractor={(item) => item.$id} renderItem={({ item }) => {
+          return (
+            <VideoCard video={item} />
+          )
+        }} ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between mb-6 items-start flex-row">
+              <View>
+                <Text className="text-sm font-pmedium text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Aabiskar dhenga              </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image source={images.logoSmall} className="h-10 w-9" resizeMode='content' />
+
+              </View>
+
+            </View>
+            <SearchInput />
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 font-pregular mb-3 text-lg">
+                Latest Videos
               </Text>
-              <Text className="text-2xl font-psemibold text-white">
-                Aabiskar dhenga              </Text>
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
             </View>
-            <View className="mt-1.5">
-              <Image source={images.logoSmall} className="h-10 w-9" resizeMode='content' />
-
-            </View>
-
           </View>
-          <SearchInput />
-          <View className="w-full flex-1 pt-5 pb-8">
-            <Text className="text-gray-100 font-pregular mb-3 text-lg">
-              Latest Videos
-            </Text>
-<Trending posts={[{id:1}, {id:2}, {id:3}]?? []}/>
-          </View>
-        </View>
-      )} ListEmptyComponent={()=>(
+        )} ListEmptyComponent={() => (
 
-       <Emptystate title="No videos Found" subtitle="Be the first one to upload the video"/>
-      )} refreshControl={<RefreshControl/>} refreshing={refreshing} onRefresh={onRefresh} />
+          <Emptystate title="No videos Found" subtitle="Be the first one to upload the video" />
+        )} refreshControl={<RefreshControl />} refreshing={refreshing} onRefresh={onRefresh} />
     </SafeAreaView>
   )
 }
--- components/VideoCard.js
+++ components/VideoCard.js
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '../constants'
 
-const VideoCard = ({ video: { title, thumnail, prompt, video, users: { email, username } } }) => {
+
+const VideoCard = ({ video: { title, thumbnail, prompt, video, users: { email, username, avatar } } }) => {
+
+    const [play, setPlay] = useState(false)
     return (
-        <View className="flex-col items-center ">
-            <Text></Text>
+        <View className="flex-col items-center px-4 mb-14 ">
+            <View className="flex-row items-start gap-3">
+                <View className="justify-center items-center flex-row flex-1">
+
+                    <View className="w-[46px] h-[46px] rounded-lg border  border-secondary justify-center items-center p-0.5">
+
+                        <Image source={{ uri: avatar }} className="w-full h-full rounded-lg" resizeMode='cover' />
+                    </View>
+                    <View className="flex-1 justify-center gap-y-1 ml-3 text-white font-psemibold text-sm ">
+
+                        <Text className="text-sm font-psemibold text-white" numberOfLines={1}>{title}</Text>
+                        <Text className="font-pregular text-gray-100 text-xs" numberOfLines={1}>
+                            {username}
+                        </Text>
+
+                    </View>
+                </View>
+                <View className="pt-2">
+                    <Image className="w-5 h-5" source={icons.menu} resizeMode='contain' />
+                </View>
+
+            </View>
+            {play ?
+                <Text className="text-white">
+                    Video Playing
+
+                </Text>
+
+
+                : <TouchableOpacity activeOpacity={0.7} onPress={() => setPlay(true)} className="h-60 w-full rounded-xl relative  justify-center items-center ">
+                    <Image source={{ uri: thumbnail }} className="w-full h-full rounded-xl mt-3 " resizeMode='cover' />
+                    <Image source={icons.play} className="w-12 h-12 absolute " resizeMode='contain' />
+                </TouchableOpacity>}
         </View>
     )
 }
